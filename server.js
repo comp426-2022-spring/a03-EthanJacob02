d094fb278cc4f0fe0f0150a49faff56c9caf7c51
EthanJacob02 <ethanj02@email.unc.edu>
2022-04-12 16:49:27 -0400

    API endpoints implemented.

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
 const express = require('express')
 const app = express()
-const args = require('minimist')(process.argv.slice(2))
 
-const port = args.port || 5000
+const port = 5000
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
-// Default response for any other request
-app.use(function(req, res) {
-    res.status(404).send('404 NOT FOUND')
-    res.type('text/plain')
-})
-
-app.get('/app/', (req, res) => {
+app.get('/app', (req, res) => {
     // Respond with status 200
     res.statusCode = 200;
     // Respond with status message "OK"
     res.statusMessage = 'OK';
     res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage)
+    res.status(res.statusCode).end(res.statusMessage)
 })
 
-app.get('/app/flip/', (req, res) => {
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
     res.type('json')
-    res.json({"flip": coinFlip()})
+    res.status(200).json({"flip": flip})
 })
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = coinFlips(req.params.number)
-    const count = countFlips(flips)
+    var flips = coinFlips(req.params.number)
+    var count = countFlips(flips)
     res.type('json')
     res.json({"raw": flips, "summary": count})
 })
@@ -44,6 +38,12 @@ app.get('/app/flip/call/heads', (req, res) => {
 app.get('/app/flip/call/tails', (req, res) => {
     res.type('json')
     res.json(flipACoin("tails"))
+})
+
+// Default response for any other request
+app.use(function(req, res) {
+    res.type('text/plain')
+    res.status(404).send('404 NOT FOUND')
 })
 
 // Coin Flip Functions
